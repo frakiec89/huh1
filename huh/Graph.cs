c8d3a014765e7794bfc823a8fa675cf25695b03e
cabotik <cabotik@DESKTOP-G3ILRCA>
2024-04-12 22:20:09 +0300

    Добавьте файлы проекта.

---
+++ huh/Graph.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace huh
+{
+    class Graph
+    {
+        public string graphType { get; set; }
+        public int graphField { get; set; }
+        public float graphValue { get; set; }
+        public string graphName { get; set; }
+
+    }
+}
---
+++ huh/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace huh
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            FirstSP.Visibility = Visibility.Collapsed;
+            SettingsSP.Visibility = Visibility.Visible;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnPChart_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        private void btnHGrahp_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnVGraph_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
